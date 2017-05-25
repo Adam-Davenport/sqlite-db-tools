aac3e5c4ae5a3f17c8d6fc8c61050189d6e571dd
Adam-Davenport <adavenpo@purdue.edu>
2017-05-25 17:39:43 -0400

    Creating basic package for db tools

---
+++ data-migration/__init__.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+import sys
+import os
+
+
+def copy_table(src_table, src_db, dest_table, dest_db):
+    source = open_connection(src_db)
+    dest = open_connection(dest_db)
+    print('Copying data from db {} to db {}.'.format(src_db, dest_db))
+
+
+def open_connection(db_location):
+    db = sqlite3.connect(db_location)
+    print('Opened database: ' % db_location)
