3b2b84cb7d882a3c8cdb62c3151c448f9f5d5ec3
Adam-Davenport <adavenpo@purdue.edu>
2017-05-25 17:36:27 -0400

    Creating setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='sqlite-db-tools',
+    description='Tools for managing sqlite3 databases.',
+    url='https://github.com/Adam-Davenport/sqlite-db-tools',
+    author='Adam Davenport',
+    license='MIT',
+    packages=['data-migration'],
+    zip_safe=False)
