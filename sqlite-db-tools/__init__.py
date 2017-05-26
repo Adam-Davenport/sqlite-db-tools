641d9744e86fe286bbb5dee963ab83396cd962ec
Adam-Davenport <adavenpo@purdue.edu>
2017-05-25 20:00:53 -0400

    Renaming folder

--- sqlite-db-tools/__init__.py
+++ sqlite-db-tools/__init__.py
@@ -8,6 +8,7 @@ def copy_table(src_table, src_db, dest_table, dest_db):
     source = open_connection(src_db)
     dest = open_connection(dest_db)
     print('Copying data from db {} to db {}.'.format(src_db, dest_db))
+    src_data = source.execute('select * from ' + src_table)
 
 
 def open_connection(db_location):
