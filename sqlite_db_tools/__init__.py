d442e47a815a827aaf0855735d64c8245ccce6a3
Adam-Davenport <adavenpo@purdue.edu>
2017-05-26 12:08:46 -0400

    Creating copy sql statements

--- sqlite_db_tools/__init__.py
+++ sqlite_db_tools/__init__.py
@@ -9,6 +9,12 @@ def copy_table(src_table, src_db, dest_table, dest_db):
     dest = open_connection(dest_db)
     print('Copying data from db {} to db {}.'.format(src_db, dest_db))
     src_data = source.execute('select * from ' + src_table)
+    dest_data = dest.cursor()
+    for row in src_data.fetchall():
+        ins = 'insert into {} values ({})'.format(
+          dest_table, ','.join(str(c) for c in row)
+        )
+        dest.execute(ins)
 
 
 def open_connection(db_location):
