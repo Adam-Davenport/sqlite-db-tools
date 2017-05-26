81bf95458bad7e2e3a274a6e3406ab5d6ced6ce2
Adam-Davenport <adavenpo@purdue.edu>
2017-05-26 15:19:37 -0400

    Addign missing return statement

--- sqlite_db_tools/__init__.py
+++ sqlite_db_tools/__init__.py
@@ -11,15 +11,17 @@ def copy_table(src_table, src_db, dest_table, dest_db):
     src_data = source.execute('select * from ' + src_table)
     dest_data = dest.cursor()
     for row in src_data.fetchall():
+        values = ','.join(str(c) for c in row)
         ins = 'insert into {} values ({})'.format(
-          dest_table, ','.join(str(c) for c in row)
+          dest_table, values
         )
         dest.execute(ins)
 
 
 def open_connection(db_location):
     db = sqlite3.connect(db_location)
-    print('Opened database: ' % db_location)
+    print('Opened database: {}'.format(db_location))
+    return db
 
 
 class Copier():
