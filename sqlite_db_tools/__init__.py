085b4ad416e3b4387520997fb315701403cb25b8
Adam-Davenport <adavenpo@purdue.edu>
2017-05-27 12:06:35 -0400

    Removing function copier

--- sqlite_db_tools/__init__.py
+++ sqlite_db_tools/__init__.py
@@ -3,24 +3,6 @@ import sys
 import os
 
 
-def copy_table(src_table, src_db, dest_table, dest_db):
-    source = open_connection(src_db)
-    dest = open_connection(dest_db)
-    print('Copying data from db {} to db {}.'.format(src_db, dest_db))
-    src_data = source.execute('select * from ' + src_table)
-    for row in src_data.fetchall():
-        cols = tuple([k for k in row.keys()])
-        # Create basic insert statement that will be populated with values
-        ins = 'INSERT OR REPLACE INTO {} {} VALUES ({})'.format(
-            dest_table, cols, ','.join(['?'] * len(cols))
-        )
-        values = [row[c] for c in cols]
-        dest.execute(ins, values)
-    dest.commit()
-    source.close()
-    dest.close()
-
-
 def open_connection(db_location):
     db = sqlite3.connect(db_location)
     # Let row be dict/tuple type
