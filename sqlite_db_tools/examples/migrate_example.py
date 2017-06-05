5292bef037f0364e653737217337026f6341ce42
Adam-Davenport <adavenpo@purdue.edu>
2017-06-05 19:19:49 -0400

    Creating internal migration example for documentation

--- sqlite_db_tools/examples/migrate_example.py
+++ sqlite_db_tools/examples/migrate_example.py
@@ -17,3 +17,15 @@ migration.copy_table()
 # Internal Migration
 
 from sqlite_db_tools.migrate import Internal_Migration
+
+db = 'path_to_db'
+table1 = 'table1'
+table2 = 'table2'
+migration = Internal_Migration(db, table1, table2)
+
+# If you have an autoincrementing field
+migration.autoincrement = True
+# Autoincrementing field name, default is id
+migration.auto_field = 'auto_field name'
+# Execute migration of table data
+migration.copy_table()
